3f883670f35e14e40731fc474c5524ba00c4097c
Zaid Hilman <mannmarpaung@gmail.com>
2024-03-20 13:07:19 +0700

    4

--- resources/views/frontend/news/detail-category.blade.php
+++ resources/views/frontend/news/detail-category.blade.php
@@ -55,7 +55,7 @@
                             @endforeach
                         </ul>
 
-                        {{-- 3 --}}
+                        {{-- 4 --}}
                         <div class="tab-content" id="pills-tabContent">
 
                             @foreach ($category as $index => $row)
